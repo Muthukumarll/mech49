b698252b931fb71926b6f543f645f539940d06ce
Muthukumarll <35914910+Muthukumarll@users.noreply.github.com>
2018-02-07 04:04:21 -0500

    program6.c

--- program6.c
+++ program6.c
@@ -1,6 +1,6 @@
 # include <stdio.h> 
 # include <conio.h>
-void main() 
+int main() 
 { 
  int i, n, d ; 
  clrscr() ; 
@@ -11,6 +11,7 @@ void main()
  printf("\nThe numbers divisible by %d are :\n\n", d) ; 
  for(i = 1 ; i <= n ; i++) 
   if(i % d == 0) 
-   printf("%d\t", i) ; 
+  printf("%d\t", i) ; 
  getch() ; 
+ return 0;
 }
