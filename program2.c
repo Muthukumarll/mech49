f58374932da834f7402ad38a87eb7a0610f6d74a
Muthukumarll <35914910+Muthukumarll@users.noreply.github.com>
2018-02-07 03:38:19 -0500

    program2.c

--- program2.c
+++ program2.c
@@ -10,22 +10,20 @@ int main()
 
     while (low < high)
     {
-        flag = 0;
+     flag = 0;
+     for(i = 2; i <= low/2; ++i)
+     {
+     if(low % i == 0)
+     {
+     flag = 1;
+       break;
+       }
+     }
 
-        for(i = 2; i <= low/2; ++i)
-        {
-            if(low % i == 0)
-            {
-                flag = 1;
-                break;
-            }
-        }
+   if (flag == 0)
+    printf("%d ", low);
 
-        if (flag == 0)
-            printf("%d ", low);
-
-        ++low;
+    ++low;
     }
-
     return 0;
 }
